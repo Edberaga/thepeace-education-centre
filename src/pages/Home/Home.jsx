c0906af002f545c0ee00beaf6bb26a4e77298015
Edberaga <edbetjonnathan@gmail.com>
2022-10-08 22:51:24 +0800

    add home folder component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Home = () => {
+    return (
+        <>
+            <header className="banner">
+                <h1 className="banner-title">
+                    Test
+                </h1>
+            </header>
+        </>
+    )
+}
