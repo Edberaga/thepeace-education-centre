80f6b576956b2cdc843e544e28d7b6d0a73b12c1
Edberaga <edbetjonnathan@gmail.com>
2022-10-08 22:50:09 +0800

    change the home destination

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 //import the pages
-import { Home } from './pages/Home'
+import { Home } from './pages/Home/Home'
 import { About } from "./pages/About";
 import { Blog } from "./pages/Blog";
 import { Donation } from "./pages/Donation";
