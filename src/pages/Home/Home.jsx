31b0e4a7408ea418a9cc504081536f5545aceed6
Edberaga <edbetjonnathan@gmail.com>
2022-10-11 23:41:03 +0800

    Banners code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import './Home.css';
+import {FaHandsHelping} from 'react-icons/fa'
+import {MdReadMore} from 'react-icons/md'
+import { Link } from "react-router-dom";
 
 export const Home = () => {
     return (
         <>
             <header className="banner">
                 <h1 className="banner-title">
-                    Test
+                    School that creates Hope
                 </h1>
+                <p className="banner-quote">
+                    Every <span className="banner-quote-1">Children</span> Should have the opportunity to receive
+                    quality <span className="banner-quote-2">Education</span> 
+                </p>
+                <div className="banner-btns">
+                    <button className="btn-learn-more">
+                        <Link to="./about">                       
+                            <MdReadMore/>
+                            <span>Learn More</span>
+                        </Link>
+                    </button>
+                    <button className="btn-volunteer-now">
+                        <Link to="./contact">                       
+                            <FaHandsHelping/>
+                            <span>Volunteer Now</span>
+                        </Link>
+                    </button>
+                </div>
             </header>
+
+            <section className="feature-one">
+                
+            </section>
         </>
     )
 }
