32bf09322d11696bd735264f49bd0ed43ad9a3e4
Edberaga <edbetjonnathan@gmail.com>
2022-10-12 23:34:16 +0800

    Update Home componment

---
+++ src/pages/Home/HomeData.js
@@ -0,0 +1,20 @@
+export const HomeData = [
+    {
+        "image": require("../../assets/icons/volunteer-icon.png"),
+        "title": "Become Volunteer",
+        "word" : "We need help, please volunteer",
+        "class": "icon-col"
+    },
+    {
+        "image": require("../../assets/icons/certificate-icon.png"),
+        "title": "Get Certificate",
+        "word" : "We need help, please volunteer",
+        "class": "icon-col"
+    },
+    {
+        "image": require("../../assets/icons/donatee-icon.png"),
+        "title": "Start Donating",
+        "word" : "We need help, please volunteer",
+        "class": "icon-col"
+    }
+];
