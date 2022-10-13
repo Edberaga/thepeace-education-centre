c0499a67e2c486639961c0f958a23f571f87ce92
Edberaga <edbetjonnathan@gmail.com>
2022-10-13 23:32:46 +0800

    Add CSS for the bacground

--- src/pages/Home/HomeData.js
+++ src/pages/Home/HomeData.js
@@ -2,19 +2,19 @@ export const HomeData = [
     {
         "image": require("../../assets/icons/volunteer-icon.png"),
         "title": "Become Volunteer",
-        "word" : "We need help, please volunteer",
-        "class": "icon-col"
+        "word" : "There are many of lorem Ipsum, but the majori have suffered alteration in some form.",
+        "class": "icon-col red-col"
     },
     {
         "image": require("../../assets/icons/certificate-icon.png"),
         "title": "Get Certificate",
-        "word" : "We need help, please volunteer",
-        "class": "icon-col"
+        "word" : "There are many of lorem Ipsum, but the majori have suffered alteration in some form.",
+        "class": "icon-col blue-col"
     },
     {
         "image": require("../../assets/icons/donatee-icon.png"),
         "title": "Start Donating",
-        "word" : "We need help, please volunteer",
-        "class": "icon-col"
+        "word" : "There are many of lorem Ipsum, but the majori have suffered alteration in some form.",
+        "class": "icon-col green-col"
     }
 ];
