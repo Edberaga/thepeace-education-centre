b003e71f9f1edb67a079c107b9279ebfb295c6e3
Edberaga <edbetjonnathan@gmail.com>
2022-10-25 23:55:04 +0800

    Add stripe container

---
+++ web/src/pages/Donate/StripeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/react-stripe-js'
+
+const STIPE_PUBLIC_KEY = "pk_test_51LwLEaIoP2MwfIyQTfzbwbWOhbeU2jcI0YpCQTNSQJtlBEvam0TqPy2nsFMGheb1OueYmUfZ958JSj49Cgyn3dmY00hQtAbPhL"
+const stripeTestPromise = loadStripe(STIPE_PUBLIC_KEY)
+
+export default function StripeContainer() {
+  return (
+    <Elements stripe={stripeTestPromise}>
+
+    </Elements>
+  )
+}
