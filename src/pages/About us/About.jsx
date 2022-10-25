8e3ea67e77997efcd069e42e2b4519a273b2501b
Edberaga <edbetjonnathan@gmail.com>
2022-10-25 22:34:12 +0800

    Add the About us content

--- src/pages/About us/About.jsx
+++ src/pages/About us/About.jsx
@@ -4,17 +4,14 @@ import Quote from '../../common.json'
 export const About = () => {
     return (
         <>
-            <section className="about-title">
-                <h1>School Mission</h1>
-            </section>
-
             <section className="about-mission">
                 <div className="container">
                     <div className="about-mission-1">
+                        <h1>School Mission</h1>
                         <p>{Quote["our-goal-about"]}</p>
                     </div>
                     <div className="about-mission-2">
-                        <img src="" className="" alt="" srcset="" />
+                        <img src={require('../../assets/img/school_mission.jpeg')} className="about-mission-img" alt="" srcset="" />
                     </div>
                 </div>
             </section>
@@ -22,10 +19,13 @@ export const About = () => {
             <section className="about-history">
                 <div className="container">
                     <div className="about-history-1">
-                        <img src="" className="" alt="" srcset="" />
+                        <img src={require('../../assets/img/school_history.jpg')} className="about-history-img" alt="" srcset="" />
                     </div>
                     <div className="about-history-2">
-                        
+                        <h1>History</h1>
+                        <p>{Quote["our-history-1"]}</p>
+                        <p>{Quote["our-history-2"]}</p>
+                        <p>{Quote["our-history-3"]}</p>
                     </div>
                 </div>
             </section>
