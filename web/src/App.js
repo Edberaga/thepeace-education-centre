b64ec20c1fccbc1672d190344967b49d057d0ae6
Edberaga <edbetjonnathan@gmail.com>
2022-10-27 23:47:41 +0800

    #

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 //import the pages
 import { Home } from './pages/Home/Home'
 import { About } from "./pages/About us/About";
-import { Blog } from "./pages/Blog";
+import { Blog } from "./pages/Blog/Blog";
 import { Donation } from "./pages/Donate/Donation";
 import { Gallery } from "./pages/Gallery";
 import { Contact } from "./pages/Contact";
@@ -25,7 +25,7 @@ function App() {
           <Route path='/blog' element={<Blog/>} />
           <Route path='/gallery' element={<Gallery/>} />
           <Route path='/contact' element={<Contact/>} />
-
+          
           {/*Registeration Pages */}
           <Route path='/signup' element={<Register/>} />
 
