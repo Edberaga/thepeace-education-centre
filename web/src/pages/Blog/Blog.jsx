aaef7646405732fa114f4f7becb0c3f3ee87d417
Edberaga <edbetjonnathan@gmail.com>
2022-10-27 23:45:28 +0800

    Debug the blog page

---
+++ web/src/pages/Blog/Blog.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {Container} from 'reactstrap'
+import ArticleCard from "../../components/ArticleCard/ArticleCard";
+import firebase from "../../config/firebase";
+
+const db = firebase.firestore()
+
+import React from 'react'
+
+export const Blog = () => {
+  return (
+    <div>Blog</div>
+  )
+}
