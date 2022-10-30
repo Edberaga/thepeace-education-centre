6ef8ca854584a5629e1d73a21811ed3d60f0b0fe
Edberaga <edbetjonnathan@gmail.com>
2022-10-30 15:26:17 +0800

    Add the function to remove an article

--- web/src/components/Article/Articles.jsx
+++ web/src/components/Article/Articles.jsx
@@ -4,6 +4,7 @@ import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 
 import './Articles.css'
 import { FaHeart } from "react-icons/fa";
+import DeleteArticle from "./DeleteArticle";
 
 export default function Articles() {
   const [articles, setArticles] = useState([]);
@@ -21,33 +22,34 @@ export default function Articles() {
   },[]);
   return (
   <>
-    <div className="container-section-full">
-        {
-            articles.length === 0 ? (
-                <h1>No Articles found!</h1>
-            ):(
-                articles.map(({id, title, content, image, createAt}) => (
-                  <div className="news-card" key={id}>
-                    <div className="news-image" style={{backgroundImage: `url(${image})`} }>
-                    </div>
-                    <div className="news-word">
-                        <p className="news-date">{createAt.toDate().toDateString()}</p>
-                        <h1>{title}</h1>
-                        <p>{content}</p>
-                    </div>
-                    <div className="news-media">
-                        <p className="news-comment">
-                            <span>0</span> comments
-                        </p>
-                        <p className="news-like">
-                            <FaHeart/> <span>0</span>
-                        </p>
-                    </div>
-                  </div>
-                ))
-            )
-        }
-    </div>
+  {
+    articles.length === 0 ? (
+    <h1>No Articles found!</h1>
+  ):(
+    articles.map(({id, title, content, image, createAt}) => (
+      <div className="news-card" key={id}>
+        <div className="news-image" style={{backgroundImage: `url(${image})`} }>
+        </div>
+        <div className="news-word">
+            <p className="news-date">{createAt.toDate().toDateString()}</p>
+            <h1>{title}</h1>
+            <p className="news-content">{content}</p>
+        </div>
+        <div className="news-media">
+            <p className="news-comment">
+                <span>0</span> comments
+            </p>
+            <p className="news-like">
+                <FaHeart/> <span>0</span>
+            </p>
+        </div>
+        <div className="news-admin-control">
+          <DeleteArticle id={id} image={image}/>
+        </div>
+      </div>
+      ))
+    )
+  }
   </>
   )
 }
---
+++ web/src/components/Article/DeleteArticle.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { db, storage } from '../../config/firebase';
+import { deleteObject, ref } from 'firebase/storage';
+
+
+function DeleteArticle({id, image}) {
+    const handleDelete = async() => {
+        try {
+            await deleteDoc(doc(db, "Articles", id))
+            alert("Article Deleted Successfully")
+            
+            //Delete the image from the storage
+            const storageRef = ref(storage, image)
+            await deleteObject(storageRef)
+        } 
+        catch(error) {
+            alert("Error Deleting Article...")
+            console.log(error)
+        }
+    }
+    
+  return (
+    <div>
+        <button className="btn btn-danger" onClick={handleDelete}>Delete</button>
+    </div>
+  )
+}
+
+export default DeleteArticle
