f2ea86ccd0ca116016c14ced63bab29a2719370d
Edberaga <edbetjonnathan@gmail.com>
2022-10-30 15:26:32 +0800

    Edit the blogpage layout

--- web/src/pages/Blog/Blog.jsx
+++ web/src/pages/Blog/Blog.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import Articles from "../../components/Article/Articles";
 import AddArticle from "../../components/Article/AddArticle";
 
+import './Blog.css'
+
 export const Blog = () => {
 
   return (
     <>
-    <div className="container-section-full">
-        <div>
-            <Articles/>
-        </div>
-        <div>
-            <AddArticle/>
-        </div>
+    <div>
+      <div className="container-article">
+        <Articles/>
+      </div>
+    </div>
+    <div>
+      <div>
+        <AddArticle/>
+      </div>
     </div>
     </>
   )
