8c39b16369de5c335b1140e2dbfeaf8a38b3b4da
Edberaga <edbetjonnathan@gmail.com>
2022-11-01 22:31:07 +0800

    Add feature to confirm when deleting article

--- src/components/Article/DeleteArticle.jsx
+++ src/components/Article/DeleteArticle.jsx
@@ -6,17 +6,19 @@ import { deleteObject, ref } from 'firebase/storage';
 
 function DeleteArticle({id, image}) {
     const handleDelete = async() => {
-        try {
-            await deleteDoc(doc(db, "Articles", id))
-            alert("Article Deleted Successfully")
-            
-            //Delete the image from the storage
-            const storageRef = ref(storage, image)
-            await deleteObject(storageRef)
-        } 
-        catch(error) {
-            alert("Error Deleting Article...")
-            console.log(error)
+        if(window.confirm("Are you sure you want to delete this Article?")) {
+            try {
+                await deleteDoc(doc(db, "Articles", id))
+                alert("Article Deleted Successfully")
+                
+                //Delete the image from the storage
+                const storageRef = ref(storage, image)
+                await deleteObject(storageRef)
+            } 
+            catch(error) {
+                alert("Error Deleting Article...")
+                console.log(error)
+            }
         }
     }
     
