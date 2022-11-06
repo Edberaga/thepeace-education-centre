9ad9b2d1b5813fe5aa3f91264f65653c89cad4a3
Edberaga <edbetjonnathan@gmail.com>
2022-11-07 00:21:50 +0800

    redesign the heading

--- src/pages/Others/errorPage.jsx
+++ src/pages/Others/errorPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function ErrorPage() {
   return (
-    <div>Error: Page can't be founded...</div>
+    <>    
+    <div className="container-heading"><h1>Error: Page can't be founded...</h1></div>
+    </>
+
   )
 }
 
