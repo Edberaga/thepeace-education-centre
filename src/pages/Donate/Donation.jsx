2471299ea9783a9c4b0f1de589b386a80df7c9f0
Edberaga <edbetjonnathan@gmail.com>
2022-11-07 00:21:25 +0800

    implement the donation heading style

--- src/pages/Donate/Donation.jsx
+++ src/pages/Donate/Donation.jsx
@@ -4,9 +4,7 @@ import './Donation.css';
 export const Donation = () => {
     return (
         <>
-            <div className="container-section">
-                
-            </div>
+        <div className="container-heading"><h1>Donation</h1></div>
         </>
     )
 }
