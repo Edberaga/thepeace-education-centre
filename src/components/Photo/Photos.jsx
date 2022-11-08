76199c9aafc8a9d53fe55c1862628d77da0ffdae
Edberaga <edbetjonnathan@gmail.com>
2022-11-08 23:09:12 +0800

    Add the like and delete photo component

--- src/components/Photo/Photos.jsx
+++ src/components/Photo/Photos.jsx
@@ -4,6 +4,8 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../../config/firebase'
 import { Link } from 'react-router-dom';
 import './Photos.css';
+import LikePhoto from './LikePhoto';
+import { DeletePhoto } from './DeletePhoto';
 
 export default function Photos() {
     const [user] = useAuthState(auth);
@@ -28,7 +30,6 @@ export default function Photos() {
                 <h1>No Photos Available</h1>
         ) : (
         photos.map(({id, photoTitle, photoDate, photoUrl, photoLikes, photoComment}) => (
-        
         <div className="single-photo" key={id}>
             <Link><img src={photoUrl} alt={photoTitle} className="photo-img" /></Link>
             <div className="photo-overlay">
@@ -37,24 +38,23 @@ export default function Photos() {
                     <p className='photo-date'>
                         {photoDate.toDate().toDateString()}
                     </p>
-                    {/* For delete photo button
-                    user && user.uid === 'wXdT70ui90WaxRTvGBBJcVXZCnp2' ?  (
-                        <div className="news-admin-control">
-                            <DeleteArticle id={id} image={image}/>
-                        </div>
-                    ) : (
-                        ''
-                    )
-                    */}
-                    <p className='photo-media'>
+                    <div className='photo-media'>
                         <Link >
                             <p className="photo-comment">
                                 <span>{photoComment?.length}</span> comment
                             </p>
                         </Link>
+                        {/* For delete photo button*/
+                        user && user.uid === 'wXdT70ui90WaxRTvGBBJcVXZCnp2' ?  (
+                            <div className="news-admin-control">
+                                <DeletePhoto id={id} image={photoUrl}/>
+                            </div>
+                        ) : (
+                            ''
+                        )}
                         <span className="photo-like">
                         {user 
-                            ? <><span>{photoLikes?.length}</span></>
+                            ? <><span>{photoLikes?.length}</span> <LikePhoto id={id} likes={photoLikes}> </LikePhoto></>
                             : <Link to={'/login'}>
                                 <i className="fas fa-heart fa-lg"> 
                                 <span>{photoLikes?.length}</span>
@@ -62,12 +62,12 @@ export default function Photos() {
                             </Link>
                         }
                         </span>
-                    </p>
+                    </div>
                 </div>  
             </div>
         </div>
         ))
-        )}
+    )}
     </div>
     </>
   )
