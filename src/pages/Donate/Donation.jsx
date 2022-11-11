c850ec829ea6437a805168544d5512702da60834
Edberaga <edbetjonnathan@gmail.com>
2022-11-11 23:47:49 +0800

    Use the PayPalScriptProvider with the form

--- src/pages/Donate/Donation.jsx
+++ src/pages/Donate/Donation.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import React, {useState, useEffect, useRef} from "react";
 import './Donation.css';
+import DonationForm from "./DonationForm";
 
 export const Donation = () => {
     return (
         <>
         <div className="container-heading"><h1>Donation</h1></div>
+        <PayPalScriptProvider
+        options={{
+            'client-id': 'AYNMxykH9aygiV5PMmSSGZRZRTP7BLIV8XtjIM1uk6FHfEjkwATOAza3eZU_Mk4leGejrKoz-z7ERJOh',
+            components: 'buttons',
+            currency: 'MYR'
+        }}
+        >
+            <h1>Donation</h1>
+            <DonationForm/>
+        </PayPalScriptProvider>
         </>
     )
 }
