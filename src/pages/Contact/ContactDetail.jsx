fe2e196a239bc1125fbbe0c377b35686eeb2294a
Edberaga <edbetjonnathan@gmail.com>
2022-11-14 23:51:38 +0800

    Add the contact details

--- src/pages/Contact/ContactDetail.jsx
+++ src/pages/Contact/ContactDetail.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { ContactData } from './ContactData'
+import './Contact.css'
 
 export default function ContactDetail() {
   return (
     <div className='contact-detail'>
-        Contact Detail
+        {ContactData.map((item, index) => {
+        return (
+        <div className="detail-part" key={index}>
+            <h5>{item.name}</h5>
+            <p>{item.content}</p>
+        </div> 
+        );
+        })}
     </div>
   )
 }
